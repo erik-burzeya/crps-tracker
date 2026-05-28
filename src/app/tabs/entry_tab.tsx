54b783e6ec346d2b55a9ae8807b721eac37c7172
Erik J. Burzeya <erik.j.burzeya@gmail.com>
2026-05-28 21:28:20 +0200

    Add: initial commit

---
+++ src/app/tabs/entry_tab.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function EntryTab() {
+  return (
+    <View>
+      <Text>Entry funktioniert</Text>
+    </View>
+  );
+}
---
+++ src/app/tabs/new-entry.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function NewEntry() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Neuer Eintrag funktioniert</Text>
+    </View>
+  );
+}
